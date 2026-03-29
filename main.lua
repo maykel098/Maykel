ff84e9bbff114ca78e08051f19221cb479fb8935
maykel098 <maykelbarre09@gmail.com>
2026-03-29 14:31:26 -0500

    main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,6 @@ print("SCRIPT CARGADO ✅")
 
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "Test",
-    Text = "Tu script funciona 🔥",
+    Text = "Tu script funciona, eres un pro maykely vivivana una noob 🔥",
     Duration = 5
 })
