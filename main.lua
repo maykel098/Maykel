6608225e1bb7e59eb5562931111eb4eb94a7c820
maykel098 <maykelbarre09@gmail.com>
2026-03-29 17:11:08 -0500

    main.lua

--- main.lua
+++ main.lua
@@ -1,7 +1,18 @@
-print("RUNNING")
+local player = game.Players.LocalPlayer
+
 task.spawn(function()
     while true do
-        print("LOOP OK")
-        task.wait(2)
+        task.wait(1)
+        pcall(function()
+            local char = player.Character
+            if char then
+                local hum = char:FindFirstChild("Humanoid")
+                if hum then
+                    hum:MoveTo(char.HumanoidRootPart.Position + Vector3.new(5,0,0))
+                end
+            end
+        end)
     end
 end)
+
+print("AUTO MOVE OK")
