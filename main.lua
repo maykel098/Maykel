1b1bd98a8a4758a4c4ca81169df273590cfb5636
maykel098 <maykelbarre09@gmail.com>
2026-03-29 17:07:21 -0500

    main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,7 @@
-local gui = Instance.new("ScreenGui", game.Players.LocalPlayer.PlayerGui)
-gui.Name = "TEST_GUI"
-
-local frame = Instance.new("Frame", gui)
-frame.Size = UDim2.new(0,200,0,100)
-frame.Position = UDim2.new(0.5,-100,0.5,-50)
-frame.BackgroundColor3 = Color3.new(1,0,0)
-
-print("GUI creada")
+print("RUNNING")
+task.spawn(function()
+    while true do
+        print("LOOP OK")
+        task.wait(2)
+    end
+end)
